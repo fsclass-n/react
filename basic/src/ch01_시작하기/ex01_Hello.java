74ed6121379f67198c6caf62b583aea3fbf64db4
fsclass-n <>
2025-12-08 15:04:00 +0900

    헬로우자바

---
+++ basic/src/ch01_시작하기/ex01_Hello.java
@@ -0,0 +1,16 @@
+package ch01_시작하기;
+
+public class ex01_Hello {
+
+	public static void main(String[] args) {
+		// ctrl+/: 주석		
+		// F2 또는 alt+shift+R: 이름 바꾸기
+		// alt+방향키: 선택한 영역(줄 단위) 위치 이동
+		
+		// 1. sysout을 쓴 후 바로 ctrl+space: 자동 완성
+		// 2. ()괄호 안에 텍스트 입력 후
+		// 3. ctrl+F11: 실행
+		System.out.println("Hello, Java!");
+	}
+
+}
